f69d96b67d6138cf7fb7c2d34744a44916543cfe
FilipeR13 <josefilipe.r.rodrigues13@gmail.com>
2023-05-08 18:56:47 +0100

    Implemented generation of codes

--- src/Main.java
+++ src/Main.java
@@ -4,7 +4,7 @@ import java.time.Month;
 public class Main {
     public static void main(String args[]) {
         LocalDate date = LocalDate.of(2010, Month.DECEMBER,12);
-        Artigos a = new Malas(Malas.Dimensao.Grande,"Couro",date,Artigos.Estado.Novo,"Nao tem danos",0,"Mala preta de couro","Gucci",000001,109.90,80);
+        Artigos a = new Malas(Malas.Dimensao.Grande,"Couro",date,Artigos.Estado.Novo,"Nao tem danos",0,"Mala preta de couro","Gucci","000001",109.90,80);
         a.calculaDesconto();
         String[] options = {"Adicionar um Utilizador!", "Remover um Utilizador!"};
         Menu mainMenu = new Menu(options);
