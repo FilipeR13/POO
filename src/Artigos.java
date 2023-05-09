d87593ae2bd2200fb238b605ab3651fd3cb882bd
a100750 <a100750@uminho.pt>
2023-05-09 23:39:43 +0100

    user_id

--- src/Artigos.java
+++ src/Artigos.java
@@ -12,6 +12,8 @@ public abstract class Artigos implements Serializable {
     private String descricao;
     private String marca;
     private String codigo;
+    private String user_id;
+    private Transportadora transportadora;
     private double preco;
     private double preco_desconto;
 
@@ -22,16 +24,20 @@ public abstract class Artigos implements Serializable {
         marca = null;
         nDonos = 0;
         codigo = "";
+        user_id = "";
+        transportadora = null;
         preco = 0;
         preco_desconto = 0;
     }
 
-    public Artigos(Estado estado, String danos,int nDonos, String descricao, String marca, String codigo, double preco, double preco_desconto) {
+    public Artigos(Estado estado, String danos,int nDonos, String descricao, String marca, String codigo, String user_id, Transportadora transportadora, double preco, double preco_desconto) {
         this.estado = estado;
         this.danos = danos;
         this.descricao = descricao;
         this.marca = marca;
         this.codigo = codigo;
+        this.user_id = user_id;
+        this.transportadora = transportadora;
         this.nDonos = nDonos;
         this.preco = preco;
         this.preco_desconto = preco_desconto;
@@ -43,6 +49,8 @@ public abstract class Artigos implements Serializable {
         this.descricao = a.getDescricao();
         this.marca = a.getMarca();
         this.codigo = a.getCodigo();
+        this.user_id = a.getUser_id();
+        this.transportadora = a.getTransportadora();
         this.nDonos = a.getnDonos();
         this.preco = a.getPreco();
         this.preco_desconto = a.getPrecoDesconto();
@@ -51,6 +59,10 @@ public abstract class Artigos implements Serializable {
     public Estado getEstado() {
         return estado;
     }
+
+    public String getUser_id() { return user_id; }
+
+    public Transportadora getTransportadora() { return transportadora; }
 
     public String getDanos() {
         return danos;
@@ -99,6 +111,10 @@ public abstract class Artigos implements Serializable {
         this.codigo = codigo;
     }
 
+    public void setUser_id(String user_id) { this.user_id = user_id; }
+
+    public void setTransportadora(Transportadora transportadora) { this.transportadora = transportadora; }
+
     public void setPreco(double preco) {
         this.preco = preco;
     }
@@ -124,6 +140,8 @@ public abstract class Artigos implements Serializable {
                 le.getDescricao().equals(this.descricao) &&
                 le.getMarca().equals(this.marca) &&
                 le.getCodigo().equals(this.codigo) &&
+                le.getUser_id().equals(this.user_id) &&
+                le.getTransportadora().equals(this.transportadora) &&
                 le.getPreco() == this.preco &&
                 le.getPrecoDesconto() == this.preco_desconto &&
                 le.getnDonos() == this.nDonos;
@@ -138,6 +156,8 @@ public abstract class Artigos implements Serializable {
         sb.append(" Descrição: ").append(this.descricao);
         sb.append(" Marca: ").append(this.marca);
         sb.append(" Código: ").append(this.codigo);
+        sb.append(" User_id: ").append(this.user_id);
+        sb.append(" Transportadora: ").append(this.transportadora);
         sb.append(" Preço: ").append(this.preco);
         sb.append(" Preço com Desconto: ").append(this.preco_desconto).append("}");
         return sb.toString();
